ea820f86243bbf1fdc578446de5abb7bf38cd4ea
Lucyanno Frota <lucyannofrota@gmail.com>
2022-12-06 18:45:32 +0000

    ROS2 Dev tools

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ colcon build \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
+
+. install/setup.bash
---
+++ build_and_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+bash build.sh
+
+ros2 run semantic_mapping semantic_mapping_node
