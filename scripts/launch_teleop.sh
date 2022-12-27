9eadb7bacf3412122cfd232098c323c4ce6899ec
Lucyanno Frota <lucyannofrota@gmail.com>
2022-12-27 20:04:34 +0000

    Lint setup

--- scripts/launch_teleop.sh
+++ scripts/launch_teleop.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 ros2 run turtlebot3_teleop teleop_keyboard
-# -e TURTLEBOT3_MODEL=burger
--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@
 set -e
 
 if [ -f install/setup.bash ]; then source install/setup.bash; fi
-colcon test --merge-install
+colcon test --merge-install --packages-select semantic_mapping
 colcon test-result
+ament_uncrustify src/semantic_mapping --reformat
