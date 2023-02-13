e495576b7add3a9ba2b2c1f822b3897f82496c21
Lucyanno Frota <lucyannofrota@gmail.com>
2023-02-13 19:33:32 +0000

    Git modules update + Dockerfile

--- build.sh
+++ build.sh
@@ -4,14 +4,18 @@ set -e
 
 # Set the default build type
 # BUILD_TYPE=RelWithDebInfo
-source /opt/ros/foxy/setup.bash
+
+
+# source /opt/ros/foxy/setup.bash
 
 BUILD_TYPE=RelWithDebInfo
-sudo colcon build \
+colcon build \
         --merge-install \
-        --packages-select semantic_mapping classifier_wrapper \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
+source install/setup.sh
 
-source install/setup.bash
+# --packages-select semantic_mapping \
+
+# source install/setup.bash
--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 set -e
 
 vcs import < src/ros2.repos src
+vcs import < src/turtlebot3/turtlebot3.repos src
+vcs import < src/turtlebot3/turtlebot3_ci.repos src
+vcs import < src/semantic_mapping/semantic_mapping.repos src/semantic_mapping/src
+
+
+# vcs import < turtlebot3.repos
 
 # Setup turtlebot3
 # cd src/turtlebot3
-# vcs import < turtlebot3.repos
 # cd turtlebot3
 # echo Builing turtlebot3
 # BUILD_TYPE=RelWithDebInfo
@@ -19,19 +24,21 @@ vcs import < src/ros2.repos src
 # cd ../../
 
 source /opt/ros/foxy/setup.bash
+# source install/setup.sh
 
-BUILD_TYPE=RelWithDebInfo
-colcon build \
-        --merge-install \
-        --symlink-install \
-        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
-. install/setup.bash
+# BUILD_TYPE=RelWithDebInfo
+# colcon build \
+#         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
+#         -Wall -Wextra -Wpedantic
+# . install/setup.bash
 
-sudo apt-get update
+# --merge-install \
+# --symlink-install \
+
+# sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
 
-source /opt/ros/foxy/setup.bash
+# source /opt/ros/foxy/setup.bash
 # cd /
 
