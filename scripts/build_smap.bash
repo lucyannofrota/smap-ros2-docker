cc205078a8f1e66bcf30c6dd897dda0dcba80c23
Lucyanno Frota <lucyannofrota@gmail.com>
2023-03-06 16:27:48 +0000

    Blacklist build.bash implemented

--- scripts/build_smap.bash
+++ scripts/build_smap.bash
@@ -2,20 +2,49 @@
 
 set -e
 
-declare -a TARGET_PKGS=(
-        "smap"
-        "smap_sampler"
-        "smap_interfaces"
-        "smap_classification"
-        "smap_classification_wrapper"
-)
+PKG_LIST=$(ls src)
 
-#echo ${TARGET_PKGS[@]}
+BLACKLIST=()
+BLACKLIST+=("ros2.repos")
+#utils
+BLACKLIST+=("utils")
+BLACKLIST+=("dynamixel_sdk")
+BLACKLIST+=("dynamixel_sdk_custom_interfaces")
+BLACKLIST+=("hls_lfcd_lds_driver")
+BLACKLIST+=("dynamixel_sdk_examples")
+#turtlebot3
+BLACKLIST+=("turtlebot3")
+BLACKLIST+=("turtlebot3_node")
+BLACKLIST+=("turtlebot3_msgs")
+BLACKLIST+=("turtlebot3_teleop")
+BLACKLIST+=("turtlebot3_bringup")
+BLACKLIST+=("turtlebot3_example")
+BLACKLIST+=("turtlebot3_fake_node")
+BLACKLIST+=("turtlebot3_description")
+BLACKLIST+=("turtlebot3_navigation2")
+BLACKLIST+=("turtlebot3_cartographer")
+#zed
+BLACKLIST+=("zed_interfaces")
+BLACKLIST+=("zed-ros2-interfaces")
+
+#misc
+BLACKLIST+=("pkg1")
+BLACKLIST+=("pkg2")
+
+
+# Filter
+PKG_LIST=$(ls src)
+
+for del in ${BLACKLIST[@]}
+do
+        PKG_LIST=("${PKG_LIST[@]/$del}")
+done
+
 
 colcon build \
         --merge-install \
         --symlink-install \
-        --packages-select ${TARGET_PKGS[@]} \
+        --packages-select ${PKG_LIST[@]} \
         --cmake-args "-DCMAKE_BUILD_TYPE=RelWithDebInfo" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
 source install/setup.bash
