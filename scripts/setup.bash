0321a3a7e8d29caad9478148221722a3a034ab38
Lucyanno Frota <lucyannofrota@gmail.com>
2023-03-09 12:23:13 +0000

    Env name fix

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -3,14 +3,14 @@ set -e
 
 git config --global --add safe.directory '*'
 
-sudo rm -r -f build install log
-sudo rm -r -f src/turtlebot3
+rm -r -f build install log
+rm -r -f src/turtlebot3
 vcs import < src/ros2.repos src
-sudo sed -i '2,5d' src/turtlebot3/turtlebot3.repos
+sed -i '2,5d' src/turtlebot3/turtlebot3.repos
 vcs import < src/turtlebot3/turtlebot3.repos src
 vcs import < src/turtlebot3/turtlebot3_ci.repos src
 # TODO: Make the repositories public
-#vcs import < src/smap/smap.repos src/smap
+vcs import < src/smap/smap.repos src/smap
 
 
 
