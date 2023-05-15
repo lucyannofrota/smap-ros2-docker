94457b14cc40a609c9331d04f1c1401a5f0d094c
Lucyanno Frota <lucyannofrota@gmail.com>
2023-05-15 18:30:29 +0100

    Velocity parameters

--- ros2_teleop.bash
+++ ros2_teleop.bash
@@ -2,5 +2,7 @@
 
 docker exec -it p3dx bash -c "
     . /workspace/foxy/install/setup.bash &&
-    ros2 run key_teleop key_teleop -r /key_vel:=/cmd_vel
+    ros2 run key_teleop key_teleop --ros-args \
+    -p forward_rate:=0.5 -p backward_rate:=0.5 \
+    -p rotation_rate:=0.8 -r /key_vel:=/cmd_vel
 "
