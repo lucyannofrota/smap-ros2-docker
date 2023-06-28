003c215c9c42a4cf6fa88aebede8fa2643d01b82
Lucyanno Frota <lucyannofrota@gmail.com>
2023-06-28 17:25:03 +0100

    smap env

--- ros2_teleop.bash
+++ ros2_teleop.bash
@@ -3,6 +3,6 @@
 docker exec -it p3dx bash -c "
     . /workspace/foxy/install/setup.bash &&
     ros2 run key_teleop key_teleop --ros-args \
-    -p forward_rate:=0.5 -p backward_rate:=0.5 \
-    -p rotation_rate:=0.8 -r /key_vel:=/cmd_vel
+    -p forward_rate:=0.25 -p backward_rate:=0.25 \
+    -p rotation_rate:=0.7 -r /key_vel:=/cmd_vel
 "
