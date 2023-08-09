61f1345b855570aa7a4a4202e8f94291bdaba711
Lucyanno Frota <lucyannofrota@gmail.com>
2023-08-09 14:30:47 +0100

    jetson-gpio integrated into p3dx image

--- build_publish_images.bash
+++ build_publish_images.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# sudo ls
+sudo ls
 
 cd p3dx-docker
 
@@ -9,15 +9,17 @@ compose_push () {
     # $2 tag_name
     # $3 idx
     # (docker-compose up -d $1 && docker tag $2 docker.io/lucyannofrota/$2) 2>&1 | tee stp$3.txt    
-    (docker-compose up -d $1 && docker tag $2 docker.io/lucyannofrota/$2 && docker image push docker.io/lucyannofrota/$2) 2>&1 | tee stp$3.log    
+    (docker-compose build --no-cache $1 && docker tag $2 docker.io/lucyannofrota/$2 && docker image push docker.io/lucyannofrota/$2) 2>&1 | tee stp$3.log    
 }
 
-compose_push noetic jetson:noetic 1
-compose_push noetic_foxy jetson:noetic-foxy 2
+compose_push noetic_foxy jetson-noetic-foxy:1.0 1
+compose_push noetic_foxy jetson-noetic-foxy:latest 2
 
-compose_push noetic_p3dx p3dx:noetic 3
-compose_push noetic_foxy_p3dx p3dx:noetic-foxy 4
+compose_push noetic_foxy_p3dx p3dx-noetic-foxy:1.0 3
+compose_push noetic_foxy_p3dx p3dx-noetic-foxy:latest 4
 
+
+shutdown +5
 
 # (docker-compose up -d noetic jetson_noetic && docker image push lucyannofrota/p3dx:noetic) 2>&1 | tee stp1.txt
 
