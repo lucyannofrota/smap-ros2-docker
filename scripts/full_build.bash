15c7a8e99a94f82d0c56b9998e20f854a55e2837
Lucyanno Frota <lucyannofrota@gmail.com>
2023-10-03 18:21:44 +0100

    scripts updated

--- scripts/full_build.bash
+++ scripts/full_build.bash
@@ -9,9 +9,10 @@ colcon build \
         --symlink-install \
         --cmake-clean-cache \
         --continue-on-error \
-        --allow-overriding slam_toolbox \
         --cmake-args "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic | sed 's/^/  /'
 source install/setup.bash | sed 's/^/  /'
 . install/setup.bash | sed 's/^/  /'
 echo "---</full_build.bash>---"
+
+# --allow-overriding slam_toolbox \
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -8,7 +8,7 @@ source /opt/ros/foxy/setup.bash | sed 's/^/  /'
 
 sudo apt-get update | sed 's/^/  /'
 rosdep update | sed 's/^/  /'
-rosdep install --from-paths src --ignore-src -y | sed 's/^/  /'
+rosdep install --from-paths src/smap --ignore-src -y | sed 's/^/  /'
 
 source /opt/ros/foxy/setup.bash | sed 's/^/  /'
 echo "---</setup.bash>---"
